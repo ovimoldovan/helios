d31b12b0a906c36c4b8157dfeeb54c308d91c4f9
hantescumihnea1 <hantescumihnea1@gmail.com>
2026-07-28 23:30:41 +0300

    Resolved App.tsx conflict

--- Seeagle/seeagle.client/src/App.tsx
+++ Seeagle/seeagle.client/src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css';
 import { LoginForm } from "@/features/login/components/LoginForm";
+import { Homepage } from './features/homepage/components/Homepage';
 
 function App() {
-    return (
-        <main className={'p-8'}>
-            <LoginForm />
-        </main>
-    );
+    const isAuthenticated = false;
+
+    if (isAuthenticated) {
+        return <Homepage isAuthenticated={isAuthenticated} />;
+    } else {
+        return (
+            <main className="p-8">
+                <LoginForm />
+            </main>
+        );
+    }
 }
 
 export default App;
