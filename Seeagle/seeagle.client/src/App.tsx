b0749bcb4bc98fa48091ee431bb915498db00c24
hantescumihnea1 <hantescumihnea1@gmail.com>
2026-07-30 18:43:42 +0300

    made the login button from fontpage direct to the login page

--- Seeagle/seeagle.client/src/App.tsx
+++ Seeagle/seeagle.client/src/App.tsx
@@ -1,19 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoginForm } from "@/features/login/components/LoginForm";
 import { Homepage } from './features/homepage/components/Homepage';
 
 function App() {
     const isAuthenticated = false;
 
-    if (isAuthenticated) {
-        return <Homepage isAuthenticated={isAuthenticated} />;
-    } else {
-        return (
-            <main className="p-8">
-                <LoginForm />
-            </main>
-        );
-    }
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Homepage isAuthenticated={isAuthenticated} />} />
+                <Route path='/login' element={
+                    <main className={'p-8'}>
+                        <LoginForm />
+                    </main>
+                } />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
