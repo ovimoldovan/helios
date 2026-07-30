0168d28c54494b178a98bb9b6c2883c2daf1d25b
hantescumihnea <hantescumihnea1@gmail.com>
2026-07-30 18:47:38 +0300

    H8 login frontend (#53)
    * feature: added login form and auth API client
    
    * added frontend login form with shadcn and error handling
    
    * frontend login, trying to revert commit
    
    * Revert "feature: added login form and auth API client"
    
    This reverts commit ca44276c4404de1b641ab47dbfe5bb85a4228749.
    
    # Conflicts:
    #	Seeagle/seeagle.client/src/App.tsx
    #	Seeagle/seeagle.client/src/features/login/components/LoginForm.tsx
    
    * frontend login
    
    * Resolved App.tsx conflict
    
    * Small changes on API login call and others
    
    * login messages
    
    * made the login button from fontpage direct to the login page

--- Seeagle/seeagle.client/src/App.tsx
+++ Seeagle/seeagle.client/src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginForm } from "@/features/login/components/LoginForm";
 import { Homepage } from './features/homepage/components/Homepage';
 
 function App() {
-    const isAuthenticated = true; //  true/false
+    const isAuthenticated = false;
 
-    return <Homepage isAuthenticated={isAuthenticated} />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Homepage isAuthenticated={isAuthenticated} />} />
+                <Route path='/login' element={
+                    <main className={'p-8'}>
+                        <LoginForm />
+                    </main>
+                } />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ Seeagle/seeagle.client/src/features/login/api/loginApi.ts
@@ -0,0 +1,13 @@
+﻿import { postJson } from '@/shared/api/httpClient';
+
+import type {LoginRequest, LoginResponse } from '@/shared/types/authentication';
+
+export async function loginUser(credentials: LoginRequest): Promise<LoginResponse> {
+    const response = await postJson<LoginResponse>('api/authentication/login', credentials);
+    
+    if (response.token){
+        document.cookie = `authToken=${response.token}; Path=/; Secure; SameSite=Strict;`;
+    }
+    
+    return response;
+}
