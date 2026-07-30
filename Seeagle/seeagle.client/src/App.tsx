9a0714c015445efd3b884f06ead6941a33c98d15
elena <elenasotoc5@gmail.com>
2026-07-30 12:43:08 +0300

    add registration route

--- Seeagle/seeagle.client/src/App.tsx
+++ Seeagle/seeagle.client/src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import { Homepage } from './features/homepage/components/Homepage';
+import { RegisterForm } from './features/registration/components/RegisterForm';
 
 function App() {
     const isAuthenticated = true; //  true/false
 
-    return <Homepage isAuthenticated={isAuthenticated} />;
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={<Homepage isAuthenticated={isAuthenticated} />}
+            />
+
+            <Route
+                path="/register"
+                element={
+                    <main className="min-h-screen p-6">
+                        <div className="mx-auto flex min-h-[calc(100vh-3rem)] max-w-7xl items-center justify-center">
+                            <RegisterForm />
+                        </div>
+                    </main>
+                }
+            />
+        </Routes>
+    );
 }
 
 export default App;
