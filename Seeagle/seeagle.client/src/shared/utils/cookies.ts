cffa7ddc295a12cacd2cf29a657a1168f6633504
madalinnegru <madalinnegru1@gmail.com>
2026-08-10 18:32:54 +0300

    Change getCookie(name: string) return type to accomodate the new get request parameter (#88)

--- Seeagle/seeagle.client/src/shared/utils/cookies.ts
+++ Seeagle/seeagle.client/src/shared/utils/cookies.ts
@@ -1,6 +1,6 @@
-﻿export function getCookie(name: string): string | null {
+﻿export function getCookie(name: string): string | undefined {
     const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
-    return match ? match[2] : null;
+    return match ? match[2] : undefined;
 }
 
 export function deleteCookie(name: string): void {
