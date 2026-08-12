cfca6052900296dcfdcc2934e6a6d7d44af90f5f
luciana morar <luciana.morar@stud.ubbcluj.ro>
2026-08-12 18:35:02 +0300

    Update reportApi.ts

--- Seeagle/seeagle.client/src/shared/api/reportApi.ts
+++ Seeagle/seeagle.client/src/shared/api/reportApi.ts
@@ -2,5 +2,5 @@ import { postJson } from './httpClient';
 import type { CreateReportRequest, Report } from "../types/report.ts";
 
 export async function createReport(data: CreateReportRequest): Promise<Report> {
-    return postJson<Report>('/api/reports', data);  // ← fără @ts-ignore
+    return postJson<Report>('/api/reports', data); 
 }
