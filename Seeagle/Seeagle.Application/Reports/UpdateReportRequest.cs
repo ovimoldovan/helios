c169a944262510814eb8db282164c79f2bb430b6
luciana morar <luciana.morar@stud.ubbcluj.ro>
2026-09-07 17:30:08 +0300

    Add MaxLength attribute to Description property

--- Seeagle/Seeagle.Application/Reports/UpdateReportRequest.cs
+++ Seeagle/Seeagle.Application/Reports/UpdateReportRequest.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Seeagle.Application.Reports;
 
 public sealed record UpdateReportRequest(
+    [MaxLength(255)]
     string? Description,
     string? Priority
 );
