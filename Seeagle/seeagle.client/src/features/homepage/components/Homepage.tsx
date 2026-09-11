8a9dce41b57eb88cda9174670586c9f724870594
elena <elenasotoc5@gmail.com>
2026-09-11 10:04:20 +0300

    fix bug

--- Seeagle/seeagle.client/src/features/homepage/components/Homepage.tsx
+++ Seeagle/seeagle.client/src/features/homepage/components/Homepage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Map } from './Map';
 import { LeftPanel } from '@/features/homepage/components/LeftPanel';
 import { AddReportModal } from '@/features/reports/components/ReportModal.tsx';
@@ -34,11 +34,15 @@ export function Homepage() {
         
     }, []);
 
-    const allReports = [...reports, ...myPendingReports];
+    const allReports = useMemo(() => {
+        const combinedReports = [...reports, ...myPendingReports];
 
-    if (selectedReport && !allReports.some(report => report.id === selectedReport.id)) {
-        allReports.push(selectedReport);
-    }
+        if (selectedReport && !combinedReports.some(report => report.id === selectedReport.id)) {
+            combinedReports.push(selectedReport);
+        }
+
+        return combinedReports;
+    }, [reports, myPendingReports, selectedReport]);
 
     const handlePinPlaced = (position: [number, number] | null) => {
         setPinPosition(position);
