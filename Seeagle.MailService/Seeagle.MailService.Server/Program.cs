038f2c5e376d4c83a2be55596b8f5b7a12e34ca8
madalinnegru <madalinnegru1@gmail.com>
2026-09-18 13:19:44 +0300

    Added required dependencies in microservice

--- Seeagle.MailService/Seeagle.MailService.Server/Program.cs
+++ Seeagle.MailService/Seeagle.MailService.Server/Program.cs
@@ -1,14 +1,36 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using Resend;
+using Seeagle.MailService.Server.Utils.MailService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<ResendSettings>(builder.Configuration.GetSection("ResendSettings"));
+
+builder.Services.AddResend(o =>
+{
+    o.ApiToken = builder.Configuration.GetSection("ResendSettings").Get<ResendSettings>()?.ApiKey
+                 ?? throw new InvalidOperationException("Resend configuration is missing");
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+var forwardedHeadersOptions = new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+};
+forwardedHeadersOptions.KnownIPNetworks.Clear();
+forwardedHeadersOptions.KnownProxies.Clear();
+app.UseForwardedHeaders(forwardedHeadersOptions);
 
 app.UseHttpsRedirection();
 
---
+++ Seeagle.MailService/Seeagle.MailService.Server/Utils/MailService/ResendSettings.cs
@@ -0,0 +1,10 @@
+namespace Seeagle.MailService.Server.Utils.MailService;
+
+public class ResendSettings
+{
+    public required string ApiKey { get; init; }
+    public required string ReportMessageTemplateId { get; init; }
+    public required string ReportUpdateWithModeratorMessageTemplateId { get; init; }
+    public required string ReportUpdateTemplateId { get; init; }
+    public required string From { get; init; }
+}
